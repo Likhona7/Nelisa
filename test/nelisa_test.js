5604404383681df86fa15d410d26c8b4e811c952
likhona <likhonapr@projectcodex.co>
2016-05-31 14:53:34 +0200

    latest

---
+++ test/nelisa_test.js
@@ -0,0 +1,15 @@
+var assert = require("assert");
+var nelisa = require("../nelisa");
+
+
+describe("Spaza Shop Data", function(){
+
+it("The most popular product sold each week", function(){
+
+
+  var week1 = new nelisa('./files/week1.csv');
+
+
+})
+
+})
