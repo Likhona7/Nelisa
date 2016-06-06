ff85be2a042d57637a9a6a5f5d08e554330d4213
likhona <likhonapr@projectcodex.co>
2016-06-06 13:50:12 +0200

    latest

--- nelisa.js
+++ nelisa.js
@@ -1,37 +1,57 @@
+
 var fs = require("fs");
 
-
 exports.getSortedData = function(path) {
-
      var spazaString = fs.readFileSync(path, "utf8");
   var spazaString = spazaString.split("\n").splice([1]).filter(Boolean);
 
-var list = [];
+  return spazaString;
+}
 
+exports.getGroupedData = function(spazaString){
+
+var list = [];
 spazaString.forEach(function(n){
 var x = n.split(",")
 list.push(x);
 })
-
 var sortedList = list.map(function(product){
-
   return {
-    Day : product[0],
-     Dates : product[1],
-     Item : product[2],
      SoldItem : product[3],
-     SalePrice : product[4]
+Item : product[2]
+
   }
 })
-//console.log(sortedList);
-     return spazaString;
- }
 
-exports.getMostPopular = function(path) {
+soldProducts  = {};
+var arrMap =[];
+sortedList.forEach(function(data){
+var currentItem = data.Item;
+var itemSold = data.SoldItem;
+
+if(soldProducts[currentItem] === undefined){
+
+  soldProducts[currentItem] = 0;
+}
+soldProducts[currentItem] += Number(itemSold);
+
+ });
 
 
-console.log(sortedList)
+for(var key in soldProducts){
+result = {
+items : key,
+itemSold : soldProducts[key]
+}
+arrMap.push(result);
+}
+console.log(arrMap)
 
+return arrMap;
 
 
 }
+
+//exports. = function(){
+//
+// }
--- test/nelisa_test.js
+++ test/nelisa_test.js
@@ -2,6 +2,8 @@ var assert = require("assert");
 var nelisa = require("../nelisa");
 
 
+
+
 describe("Spaza Shop Data", function(){
 
 it("Sorting out the data for the week", function(){
@@ -14,10 +16,27 @@ it("Sorting out the data for the week", function(){
 
 })
 
- it("The most popular product sold each week", function(){
-   var results = nelisa.getMostPopular('./files/week1.csv');
+ it("Should group the data for each week", function(){
+   var spazaString = nelisa.getSortedData('./files/week1.csv');
+var results = nelisa.getGroupedData(spazaString);
+   var week1 = [ { items: 'Milk 1l', itemSold: 39 },
+     { items: 'Imasi', itemSold: 30 },
+     { items: 'Bread', itemSold: 45 },
+     { items: 'Chakalaka Can', itemSold: 23 },
+     { items: 'Gold Dish Vegetable Curry Can', itemSold: 17 },
+     { items: 'Fanta 500ml', itemSold: 33 },
+     { items: 'Coke 500ml', itemSold: 54 },
+     { items: 'Cream Soda 500ml', itemSold: 22 },
+     { items: 'Iwisa Pap 5kg', itemSold: 17 },
+     { items: 'Top Class Soy Mince', itemSold: 22 },
+     { items: 'Shampoo 1 litre', itemSold: 3 },
+     { items: 'Soap Bar', itemSold: 12 },
+     { items: 'Bananas - loose', itemSold: 47 },
+     { items: 'Apples - loose', itemSold: 36 },
+     { items: 'Mixed Sweets 5s', itemSold: 49 } ];
 
-           assert.equal(results, 177);
+  
+assert.deepEqual(results, week1);
 
        });
 
