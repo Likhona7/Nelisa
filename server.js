f4675f3cf45b55e00b70ee70017934f2f02f7fdb
likhona <likhonapr@projectcodex.co>
2016-07-18 10:43:32 +0200

    latest

--- server.js
+++ server.js
@@ -88,11 +88,17 @@ app.get('/sales/:week', function(req, res) {
   res.render("index", weekStat[req.params.week]);
 });
 
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-
+// var server = app.listen(3000, function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log('Example app listening at http://%s:%s', host, port);
+//
+// });
+//set the port number to an existing environment variable PORT or default to 5000
+app.set('port', (process.env.PORT || 5000));
+//start the app like this:
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
