9ce98b498ee03541a7e934b49640ccc3d498388c
likhona7 <likhona@projectcodex.co>
2016-11-17 09:28:53 +0200

    latest

--- routes/sales.js
+++ routes/sales.js
@@ -23,7 +23,8 @@ exports.searchSales = function(req, res, next) {
       if (err) return next(err);
       console.log('Record Updated ' + results +"lllllllllllllllllllllllllllllllllllllllllll");
       res.render('saleSearch', {
-        sales: results
+        sales: results,
+        layout : false
       });
     })
   })
